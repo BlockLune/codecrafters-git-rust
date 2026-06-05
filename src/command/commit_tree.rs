6e17414690f2db5ed62ba548545fd5e4b566fc04
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-06-06 00:40:02 +0800

    fix: propagate write_to_disk error in commit_tree

--- src/command/commit_tree.rs
+++ src/command/commit_tree.rs
@@ -4,7 +4,7 @@ use crate::object::commit::CommitObject;
 
 pub fn run(tree_sha: &str, parent_sha: &str, message: &str) -> Result<()> {
     let commit_obj = CommitObject::new(tree_sha, parent_sha, message);
-    commit_obj.write_to_disk();
+    commit_obj.write_to_disk()?;
     println!("{}", hex::encode(commit_obj.sha1()));
     Ok(())
 }
